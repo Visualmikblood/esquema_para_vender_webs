bc46842edc25a27c830b6512592f9b9ae1a907f9
Miguel <1bcmigueliglesias@gmail.com>
2025-10-17 17:16:02 +0200

    he creado los paquetes de webs de peluqueria y he añadido algunos contenidos para pagian inicio-home

---
+++ paquetes_para_vender.sh
@@ -0,0 +1,38 @@
+PAQUETES PELUQUERIA:
+
+1º PAQUETE BASICO:    
+    1:HOME
+    2:SOBRE NOSOTROS / HISTORIA
+    3:NUESTRO EQUIPO
+    4:NUESTROS SERVICIO
+    5:CONTACTANOS => De cinco tipos | formulario / telefono,email,calle..etc / reservar cita / reservar con calendario / localizaciones
+    6:GALERIA => De dos tipos | galeria normal / galeria portfolio  
+    
+
+2º PAQUETE INTERMEDIO:
+    1:HOME
+    2:SOBRE NOSOTROS / HISTORIA
+    3:NUESTRO EQUIPO
+    4:NUESTROS SERVICIO
+    5:CONTACTANOS => De cinco tipos | formulario / telefono,email,calle..etc / reservar cita / reservar con calendario / localizaciones
+    6:GALERIA => De dos tipos | galeria normal / galeria portfolio  
+    7:TESTIMONIOS
+    8º BLOG / NOTICIAS
+
+3º PAQUETE AVANZADO:
+    1:HOME
+    2:SOBRE NOSOTROS / HISTORIA
+    3:NUESTRO EQUIPO
+    4:NUESTROS SERVICIO
+    5:CONTACTANOS => De cinco tipos | formulario / telefono,email,calle..etc / reservar cita / reservar con calendario / localizaciones
+    6:GALERIA => De dos tipos | galeria normal / galeria portfolio  
+    7:TESTIMONIOS
+    8º BLOG / NOTICIAS
+    9º TIENDA
+    10º NEWSLETTER  
+
+    ---------------------------------------------------------------------------------------------------------------------------------------------
+
+    1º HOME: 
+        1_ PORTADA : Slider o solo foto, Con titulos llamativos y frases sobre peluqueria. Ademas tiene botones a "sobre nosotros" y "nuestros servicios" Tambien puede tener un video de fondo. Tambien puede tener un boton de "reserva tu cita" o direccion a "contactanos". Tambien peude tener dias y horas abierto
+        2_ SERVICIOS DESTACADOS: Una seccion donde se muestran los servicios mas importantes o mas solicitados. Con una foto, un titulo y una breve descripcion. Tambien puede tener un boton de "ver todos los servicios" tambien puede tener precios
